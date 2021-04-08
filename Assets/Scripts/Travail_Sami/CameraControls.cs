b6ce3734d62cc2df818fd82b8312c10e4d21fa59
Invidea <sami.torbey@e-artsup.net>
2021-04-08 09:47:11 +0200

    CameraControls w/ PlayerRotation

---
+++ Assets/Scripts/Travail_Sami/CameraControls.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+
+    public float speedH = 2f;
+    public float speedV = 2f;
+
+    public float yaw = 0f;
+    public float pitch = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X") * Time.deltaTime;
+        pitch += speedV * Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+        transform.eulerAngles = new Vector3(pitch, yaw);
+    }
+}
