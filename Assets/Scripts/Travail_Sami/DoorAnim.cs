1f2ca29aab72d9053b9de7c054e4225b8bdc4536
Invidea <sami.torbey@e-artsup.net>
2021-05-10 16:08:19 +0200

    AddSFX + ModifDoor

--- Assets/Scripts/Travail_Sami/DoorAnim.cs
+++ Assets/Scripts/Travail_Sami/DoorAnim.cs
@@ -7,11 +7,15 @@ public class DoorAnim : MonoBehaviour
 
     public Animator doorAnim;
 
+    public AudioSource[] doorSource;
+    public AudioClip[] doorClips;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             doorAnim.SetTrigger("isOpening");
+            doorSource[0].PlayOneShot(doorClips[0]);
         }
     }
 
@@ -20,6 +24,7 @@ public class DoorAnim : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             doorAnim.SetTrigger("isClosing");
+            doorSource[1].PlayOneShot(doorClips[1]);
         }
     }
 }
