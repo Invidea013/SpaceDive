18e4cb4a303dd53ca66ac12bb52edb0605279add
Invidea <sami.torbey@e-artsup.net>
2021-05-12 09:57:44 +0200

    ModifUI + Cinematic

--- Assets/Scripts/Travail_Noah/Cinematic_Config.cs
+++ Assets/Scripts/Travail_Noah/Cinematic_Config.cs
@@ -6,27 +6,53 @@ using UnityEngine.SceneManagement;
 public class Cinematic_Config : MonoBehaviour
 {
     public GameObject[] Cinematics;
+    public AudioSource[] cinematicSources;
+    public AudioClip[] cinematicClips;
+
     private float Timer;
-    // Update is called once per frame
+
+    void Start()
+    {
+        cinematicSources[0].Play();
+    }
+
     void Update()
     {
         Timer += Time.deltaTime;
-        if (Timer >= 3f)
+
+        if (Timer >= 5f)
         {
             Cinematics[0].SetActive(true);
-            
+            cinematicSources[1].Play();
         }
-        if (Timer >= 6f)
+
+        if (Timer >= 10f)
         {
             Cinematics[1].SetActive(true);
+
+            cinematicSources[1].Stop();
+            cinematicSources[2].PlayOneShot(cinematicClips[2]);
         }
-        if (Timer >= 9f)
+
+        if(Timer >= 11f)
         {
-            Cinematics[2].SetActive(true);
+            cinematicSources[3].PlayOneShot(cinematicClips[3]);
         }
-        if (Timer >= 15f)
+
+        if(Timer >= 11.5f)
         {
-            SceneManager.LoadScene(1);
+            cinematicSources[4].PlayOneShot(cinematicClips[4]);
+            cinematicSources[5].PlayOneShot(cinematicClips[5]);
+        }
+
+        if(Timer >= 12f)
+        {
+            cinematicSources[6].PlayOneShot(cinematicClips[6]);
+        }
+
+        if (Timer >= 13f)
+        {
+            SceneManager.LoadScene(2);
         }
     }
 }
