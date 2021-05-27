535e3d4cc39d4eecf0130a4e1ef0f47524b41688
Invidea <sami.torbey@e-artsup.net>
2021-05-27 11:01:25 +0200

    Habillage Finish + MiniMap Finish

---
+++ Assets/Scripts/Travail_Sami/MapPoints.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapPoints : MonoBehaviour
+{
+
+    public GameObject[] mapPoints;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Cockpit"))
+        {
+            mapPoints[0].SetActive(true);
+        }
+
+        if (other.CompareTag("Office_1"))
+        {
+            mapPoints[1].SetActive(true);
+        }
+
+        if (other.CompareTag("Office_2"))
+        {
+            mapPoints[2].SetActive(true);
+        }
+
+        if (other.CompareTag("Storage_1"))
+        {
+            mapPoints[3].SetActive(true);
+        }
+
+        if (other.CompareTag("Storage_2"))
+        {
+            mapPoints[4].SetActive(true);
+        }
+
+        if (other.CompareTag("Kitchen"))
+        {
+            mapPoints[5].SetActive(true);
+        }
+
+        if (other.CompareTag("Reactor"))
+        {
+            mapPoints[6].SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Cockpit"))
+        {
+            mapPoints[0].SetActive(false);
+        }
+
+        if (other.CompareTag("Office_1"))
+        {
+            mapPoints[1].SetActive(false);
+        }
+
+        if (other.CompareTag("Office_2"))
+        {
+            mapPoints[2].SetActive(false);
+        }
+
+        if (other.CompareTag("Storage_1"))
+        {
+            mapPoints[3].SetActive(false);
+        }
+
+        if (other.CompareTag("Storage_2"))
+        {
+            mapPoints[4].SetActive(false);
+        }
+
+        if (other.CompareTag("Kitchen"))
+        {
+            mapPoints[5].SetActive(false);
+        }
+
+        if (other.CompareTag("Reactor"))
+        {
+            mapPoints[6].SetActive(false);
+        }
+    }
+
+}
